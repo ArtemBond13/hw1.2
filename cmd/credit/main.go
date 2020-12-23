6fb58fe300dadb7184c89f4bfc35061dffeba102
Artem <garribond85@gmail.com>
2020-12-23 15:01:08 +0500

    Replace value in ExampleCalculate()

--- cmd/credit/main.go
+++ cmd/credit/main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	amount := 1_000_000_00
 	percent := 20
-	months := 36
+	months := 12
 
 	monthly, total, over := credit.Calculate(amount, percent, months)
 	fmt.Println(monthly, total, over)
